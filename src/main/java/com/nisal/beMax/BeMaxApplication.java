ee71a0e7d0cc64efdb301edf8a78940e6a136a66
NisalSandaru <nisalsandaru1@gmail.com>
2025-10-16 12:45:11 +0530

    Initial commit

---
+++ src/main/java/com/nisal/beMax/BeMaxApplication.java
@@ -0,0 +1,13 @@
+package com.nisal.beMax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisal/beMax/BeMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisal.beMax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
